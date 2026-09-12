1effb8461066d77be80872686e90631d07bfe4cd
mel0nze <richard09ad@gmail.com>
2026-09-12 17:07:35 +0300

    Added autoinstall script

---
+++ install.sh
@@ -0,0 +1,33 @@
+/bin/bash
+
+cd dwm
+
+sudo make install
+
+cd ..
+
+cd st
+
+sudo make install
+
+cd ..
+
+cd dmenu 
+
+sudo make install
+
+cd ..
+
+cd slstatus
+
+sudo make install
+
+cd ..
+
+echo "exec dwm" >> ~/.xinirc
+echo "slstatus &" >> ~/.xinirc
+sudo echo "export PS1='\[\e[38;5;175m\]\u@\h\[\e[0m\] \[\e[38;5;244m\]\W\[\e[0m\] \$ '" >> .bashrc
+
+source .bashrc
+
+echo "INSTALL FINISHED"
