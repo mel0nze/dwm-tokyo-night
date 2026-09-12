da0e9eb823a2e8f6a80cca0996c2b930999d21bf
mel0nze <richard09ad@gmail.com>
2026-09-12 18:35:56 +0300

    Auto install nerd font and labeled code in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#install WM
+
 cd dwm
 
 sudo make install
@@ -28,8 +30,22 @@ cd ..
 echo "cd" >> ~/.xinitrc
 echo "slstatus &" >> ~/.xinitrc
 echo "exec dwm" >> ~/.xinitrc
+
+#Install Nerd Font
+
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d JetBrainsMono
+rm JetBrainsMono.zip
+fc-cache -fv
+
+
+#Matching bash prompt
+
 sudo echo "export PS1='\[\e[38;5;175m\]\u@\h\[\e[0m\] \[\e[38;5;244m\]\W\[\e[0m\] \$ '" >> ~/.bashrc
 
 source ~/.bashrc
+
 
 echo "INSTALL FINISHED"
